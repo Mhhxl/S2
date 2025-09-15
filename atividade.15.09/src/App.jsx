aec2191690b3103d63cf116f07d5246bc9713957
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-15 16:11:07 -0300

    salvando

---
+++ atividade.15.09/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import InfoAluno from './components/InfoAluno'
+import ListarAluno from './components/ListarAluno'
+
+
+function App() {
+
+
+  return (
+    <>
+      <InfoAluno />
+    </>
+  );
+}
+
+export default App
